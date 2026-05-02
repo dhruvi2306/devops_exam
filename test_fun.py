13a9b4b9f25fd175f14eaf651a0d43126e91e3a0
Dhruvi <dhruvidesai3605@gmail.com>
2026-05-02 09:46:28 +0530

    Add files via upload

---
+++ test_fun.py
@@ -0,0 +1,14 @@
+
+import fun as f 
+
+def test_add():
+    assert f.add(3,5)==8
+
+def test_sub():
+    assert f.sub(5,3)==2
+
+def test_mul():
+    assert f.mul(5,3)==15
+
+def test_div():
+    return f.div(4,2)==2
